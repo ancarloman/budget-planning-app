5579908a188ebc43c2633f059bbbd205a0ec2064
Angel Nicole Carloman <angelnicole.carloman@braathedev.no>
2026-02-04 16:52:22 +0800

    Top bar updates

--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -1,23 +1,48 @@
-import { Cat, Flower2, CloudMoon } from 'lucide-react'
+import { User, Settings } from 'lucide-react'
 
 import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { ModeToggle } from '../mode-toggle';
+import { Link } from '@tanstack/react-router';
+import { SidebarTrigger } from '../ui/sidebar';
 
 function TopBar() {
   return (
     <>
-    <div className="w-full z-50 flex items-center justify-end px-4 md:px-8 py-4 bg-background gap-4">
-      <div className="flex items-center gap-4">
+    <div className="w-full z-50 flex items-center justify-between px-4 md:px-8 py-4 bg-background gap-4">
+      <div className="flex items-center">
+        <SidebarTrigger className="-ml-4 cursor-pointer" />
         <ModeToggle />
-        
-        {/* <Button variant="outline" size="icon-sm">
-          <Flower2/>  Theme toggle 
-        </Button> */}
-
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+      </div>
+      
+      <div className="flex items-center">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="bg-[var(--background)]" align="end">
+            <DropdownMenuItem className="cursor-pointer hover:text-[var(--foreground)]">
+              <Link to="/profile" className="flex items-center cursor-pointer">
+                <User className="mr-2 h-4 w-4 text-foreground" />
+                <span>Profile</span>
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer hover:text-[var(--foreground)]">
+              <Link to="/settings" className="flex items-center cursor-pointer">
+                <Settings className="mr-2 h-4 w-4 text-foreground" />
+                <span>Settings</span>
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
     </>
--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -12,18 +12,20 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon-sm" className="cursor-pointer">
+        <Button variant="ghost" size="icon-sm" className="cursor-pointer">
           <Cat className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <CloudMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="bg-[var(--background)]" align="end">
-        <DropdownMenuItem className="cursor-pointer hover:text-[var(--foreground)]">
-          DayTime
+      <DropdownMenuContent className="bg-[var(--background)]">
+        <DropdownMenuItem className="cursor-pointer text-xs hover:text-[var(--foreground)]">
+          <Cat className="h-4 w-4 text-foreground" />
+          <span>DayTime</span>
         </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer hover:text-[var(--foreground)]">
-          NightSky
+        <DropdownMenuItem className="cursor-pointer text-xs hover:text-[var(--foreground)]">
+          <CloudMoon className="h-4 w-4 text-foreground" />
+          <span>NightSky</span>
         </DropdownMenuItem>
         {/* <DropdownMenuItem onClick={() => setTheme("system")} className="cursor-pointer">
           System
